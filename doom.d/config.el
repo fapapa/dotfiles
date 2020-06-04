04c7246732dc60fc686b17801ac70cfcaa9afe5f
Fabio Papa <fabio.papa@animikii.com>
2020-06-04 09:06:08 -0700

    Change email address to animikii one

--- doom.d/config.el
+++ doom.d/config.el
@@ -7,7 +7,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Fabio Papa"
-      user-mail-address "fabtheman@gmail.com")
+      user-mail-address "fabio.papa@animikii.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
