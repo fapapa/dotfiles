4b78e0a762afab67a694e911d58a120ee8dca5e4
Fabio Papa <fabtheman@gmail.com>
2020-06-10 08:38:39 -0700

    Display logs in color

--- doom.d/config.el
+++ doom.d/config.el
@@ -80,6 +80,13 @@ With zero ARG, skip the last one and mark next"
 
 (setq! web-mode-markup-indent-offset 2)
 
+(require 'ansi-color)
+(defun display-ansi-colors ()
+  (interactive)
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max))))
+(add-to-list 'auto-mode-alist '("\\.log\\'" . display-ansi-colors))
+
 ;; Load customize variables
 (load! "custom")
 
