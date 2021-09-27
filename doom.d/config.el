a5594f5d048464408a1c5b0d401becb19a6ebde2
Fabio Papa <fabio.papa@animikii.com>
2021-09-27 09:35:06 -0700

    Emacs: Add bindings to nav between tabs

--- doom.d/config.el
+++ doom.d/config.el
@@ -27,6 +27,10 @@
   '(font-lock-comment-face :slant italic)
   '(font-lock-keyword-face :slant italic))
 
+(map! (:when IS-MAC
+        "s-}" '+workspace/switch-right
+        "s-{" '+workspace/switch-left))
+
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
